a7fbb6230a442854e503ebb8f80ad88406fa633e
Ansonsd5 <anson11sd@gmail.com>
2023-12-23 20:38:34 +0530

    feat : code exapmle for closures

--- closures.js
+++ closures.js
@@ -1,8 +1,47 @@
-function x(){
-    const a= 6;
-    function y(){
-        console.log(a);
+// function x(){
+//     var a =66;
+//    return function y(){
+//         console.log(a);
+//     }
+// }
+// var z = x();
+
+// z();
+
+// const array = [1, 2, 3];
+// let num = 1;
+// num += 1;
+// num -= 1;
+
+// const sum = array.reduce((a, b) => a + b, 0);
+// const truthyCount = array.filter(Boolean).length;
+
+// function example() {
+//     console.log(anonymous); // => undefined
+
+//     anonymous(); // => TypeError anonymous is not a function
+
+//     var anonymous = function () {
+//       console.log('anonymous function expression');
+//     };
+//   }
+
+
+function x() {
+
+    
+ 
+
+ for( var i=1; i<=5;i++){
+    function close(newi){
+        setTimeout(function () {
+            console.log(newi);
+          }, 1000 * newi);
     }
-    y();
+    close(i);
+ }
+
+ 
 }
-var z =x();
+
+x();
